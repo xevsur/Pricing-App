e31fc6e46022b665adb54b697deb709cfd37f79b
Gode gogochuri <godegogocha@gmail.com>
2023-10-24 17:59:51 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="MainDiv">
+      <div className="bgRectangle">
+        <img src="/images/pattern-circles.svg" alt="crls" />
+      </div>
+      <h1>Simple, traffic-based pricing</h1>
+      <p>Sign-up for our 30-day trial. No credit card required. </p>
+    </div>
+  );
+}
+
+export default App;
