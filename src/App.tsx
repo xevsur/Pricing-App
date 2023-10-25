9964250948477c9446a6050ff5db2349f1c260f5
Gode gogochuri <godegogocha@gmail.com>
2023-10-25 14:00:37 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import PriceSlider from "./components/PriceSlider/PriceSlider";
 import CheckBox from "./components/CheckBox/CheckBox";
